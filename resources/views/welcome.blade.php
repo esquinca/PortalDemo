a602f27945c3f6c2092619908b18ac1d539e5039
Alonso Cauich <alonsocauichv1@gmail.com>
2017-08-16 10:51:12 -0500

    import bootstrap and validator

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
         <title>Laravel</title>
 
         <!-- <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"> -->
+        <!--Import bootstrap.css-->
+        <link href="{{ asset('/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
 
         <!--Import Google Icon Font-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -18,41 +20,37 @@
          <!--Import materialize.js-->
         <script src="{{ asset('/plugins/materialize/js/materialize.js') }}" type="text/javascript"></script>
 
+        <!--Import validator.js-->
+        <script src="{{ asset('/js/validator.js') }}" type="text/javascript"></script>
+
         <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
         </style>
     </head>
     <body>
         <div class="container">
+
             <div class="content">
-                <div class="title">Laravel 5</div>
+              <div class="row">
+                  <div class="col-sm-12">
+                      <div class="white-box">
+                          <h3 class="box-title m-b-0">Iniciar sesión</h3>
+                          <p class="text-muted m-b-30"> Bootstrap Form Validation</p>
+                          <form data-toggle="validator">
+                              <div class="form-group">
+                                  <label for="inputEmail" class="control-label">Email</label>
+                                  <input type="email" class="form-control" id="inputEmail" placeholder="Email" data-error="Bruh, that email address is invalid" required>
+                                  <div class="help-block with-errors"></div>
+                              </div>
+
+                              <div class="form-group">
+                                  <button type="submit" class="btn btn-primary">Submit</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
             </div>
+
         </div>
     </body>
 </html>
