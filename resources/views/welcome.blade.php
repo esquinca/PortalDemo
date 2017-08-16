470f2c355b3fa1ab11bb39eefedc7ab152a4e5aa
Alonso Cauich <alonsocauichv1@gmail.com>
2017-08-16 10:14:48 -0500

    import materialize

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,20 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"> -->
+
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+         <!--Import materialize.css-->
+        <link href="{{ asset('/plugins/materialize/css/materialize.css') }}" rel="stylesheet" type="text/css" />
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+         <!--Import materialize.js-->
+        <script src="{{ asset('/plugins/materialize/js/materialize.js') }}" type="text/javascript"></script>
 
         <style>
             html, body {
